865736c52cab9f71de5e6b87ec818c7cd7a5911a
June YAMAMOTO <june@yambal.net>
2021-03-27 13:29:48 +0900

    0.2.7

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,4 @@
 import * as free from '../build/index'
 import { ApiClientError, HrUser } from '../build/index'
 
-free.hr.user.getMe('token')
-.then((data: HrUser) => {
-
-})
-.catch((error: ApiClientError) => {
-
-})
+//free.util.getElapsedTimeJp(new Date(), new Date())
